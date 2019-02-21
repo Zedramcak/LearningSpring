6fe54d148bc3d34748206605b7e3669b54aead73
Zedramcak <adamzrc@gmail.com>
2019-02-21 14:36:14 +0100

    Spring Dependency Injection

---
+++ src/com/luv2code/springdemo/HockeyCoach.java
@@ -0,0 +1,17 @@
+package com.luv2code.springdemo;
+
+public class HockeyCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Skate around the stadium for 7 minutes and then shoot at the net";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
